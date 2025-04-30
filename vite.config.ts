6820bec104ce648cae1a37fcde2a830c65257be8
Miles Pool <milesjpool@gmail.com>
2025-04-30 16:59:04 +0100

    fix: update build output directory to match GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/guitarex/',
+  build: {
+    outDir: 'build'
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
